0385e234c5c4af6b99ee114256577a7d2c238cbd
MOHAMMAD AKASH <mohammadakashiam@gmail.com>
2025-11-09 08:38:24 +0600

    Update toilet.js

--- Script/commands/toilet.js
+++ Script/commands/toilet.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Toilet 🚽",
-	commandCategory: "Image",
+	commandCategory: "fun",
 	usages: "rank",
 	cooldowns: 5,
 	dependencies: {
